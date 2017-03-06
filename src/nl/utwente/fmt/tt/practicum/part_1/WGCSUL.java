ff5a4fb30dbec61a2d431d6fdb2e29a80e0c55ae
djhuistra <djhuistra@gmail.com>
2017-03-06 20:33:33 +0100

    Practicum Part 2

--- src/nl/utwente/fmt/tt/practicum/part_1/WGCSUL.java
+++ src/nl/utwente/fmt/tt/practicum/part_1/WGCSUL.java
@@ -3,12 +3,17 @@ package nl.utwente.fmt.tt.practicum.part_1;
 import de.learnlib.api.SUL;
 import de.learnlib.api.SULException;
 
+/**
+ * A wrapper class for a Wolf-goat-cabbage implementation
+ * See the SUL interface for more info:
+ * https://github.com/LearnLib/learnlib/blob/develop/core/src/main/java/de/learnlib/api/SUL.java
+ */
 public class WGCSUL implements SUL<String, String> {    
     
     
 	@Override
 	public void pre() {
-		// TODO: Implement model reset
+		// TODO: Implement a model reset before each Query (sequence of steps)
 	}
 	
 	@Override
@@ -17,7 +22,7 @@ public class WGCSUL implements SUL<String, String> {
 
 	@Override
 	public String step(String input) throws SULException {
-		// TODO: Implement action, return output string.
+		// TODO: Implement single step, return output string.
 		return "";
 	}
 }
